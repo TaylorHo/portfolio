0d89cec48cf4f4ad8f5f7eefb044066e2731016d
TaylorHo <taylor@hotay.dev>
2024-11-25 12:04:52 -0300

    fix: vercel live permission for CSP

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self' fonts.gstatic.com cdn.jsdelivr.net data: use.typekit.net;
-  frame-src giscus.app
+  frame-src giscus.app vercel.live;
 `
 
 const securityHeaders = [
