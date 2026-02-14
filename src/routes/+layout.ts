3706e570dec0ad61ba72a5cb2c141aa475d79c5a
Taylor Hoffmann <taylor@hoffmann.io>
2026-02-14 14:44:01 -0300

    chore: checking if posthog is not initialized before initializing it

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,12 @@ import posthog from 'posthog-js';
 import { browser } from '$app/environment';
 
 export const load = async ({ url }) => {
-	if (browser && url.hostname !== 'localhost' && url.hostname !== '127.0.0.1') {
+	if (
+		browser &&
+		url.hostname !== 'localhost' &&
+		url.hostname !== '127.0.0.1' &&
+		!posthog.__loaded
+	) {
 		posthog.init('phc_JBYFh2Ew3kdz3hfD8IIxV62Ml7ipQnAINBYflUv2608', {
 			api_host: 'https://insights.hoffmann.io',
 			defaults: '2025-05-24'
