f0b15ee8c5eae2ef622068941dcac15b01b470a4
Taylor Hoffmann <taylor@hoffmann.io>
2026-03-02 21:20:44 -0300

    fix: UTC time correctly showing on blog posts

--- src/lib/services/date.ts
+++ src/lib/services/date.ts
@@ -1,5 +1,6 @@
 export function formatDate(date: string): string {
 	return new Date(date).toLocaleDateString(undefined, {
+		timeZone: 'UTC',
 		month: 'long',
 		day: 'numeric',
 		year: 'numeric'
